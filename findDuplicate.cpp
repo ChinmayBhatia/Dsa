30d132545778bc0f3251bd4cac0043adc363cae8
chinmay bhatia <chinmaybhatia7@gmail.com>
2026-06-02 21:23:41 +0530

    find duplicate

---
+++ findDuplicate.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 3, 4, 2, 2};
+
+    Solution obj;
+    cout << obj.findDuplicate(nums) << endl;
+
+    return 0;
+}
